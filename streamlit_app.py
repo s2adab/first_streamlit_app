f22cff42556ec80f11d27a7daf2bc0df0882d9da
s2adab <86416394+s2adab@users.noreply.github.com>
2023-02-24 11:49:13 +0100

    Update streamlit_app.py
    Adding the dataframe of the fruits nutrition facts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 &  Blueberry Oatmeal')
@@ -6,3 +9,5 @@ streamlit.text('Kale, 🥗 Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞avocado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+streamlit.dataframe(my_fruit_list)
