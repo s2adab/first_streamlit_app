2769829d37e2e4d545c3bc4851891fd616b391ef
s2adab <86416394+s2adab@users.noreply.github.com>
2023-02-28 13:44:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,14 @@
 import streamlit
 import pandas 
 import requests 
+import snowflake.connector
+import requests
+
 
 streamlit.header('Fruity fruit advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"watermelon")
 
 # take the json file and normalize it  
