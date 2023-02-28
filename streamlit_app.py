18127c4f41ffbcee23db6a590323cda789b2b2c2
s2adab <86416394+s2adab@users.noreply.github.com>
2023-02-28 10:27:42 +0100

    Update streamlit_app.py
    Adding the choice question

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@ import streamlit
 import pandas 
 import requests 
 
+streamlit.header('Fruity fruit advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"watermelon")
 
