fe6d0bfb0af8d7219055bea34454fa93dc32cd89
s2adab <86416394+s2adab@users.noreply.github.com>
2023-02-28 10:08:17 +0100

    Update streamlit_app.py
    Using the request library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,7 @@
 import streamlit
 import pandas 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+import requests 
 
-streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 &  Blueberry Oatmeal')
-streamlit.text('Kale, 🥗 Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞avocado toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
