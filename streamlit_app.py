c8401971f68cd0cc490b05302c5d1a3a831bc9cd
s2adab <86416394+s2adab@users.noreply.github.com>
2023-02-28 10:25:34 +0100

    Update streamlit_app.py
    Updating the ui

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import pandas
 import requests 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"watermelon")
 
 # take the json file and normalize it  
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
