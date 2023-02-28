c74f950eaa6fa48d366905747e7d41bd706ffe17
s2adab <86416394+s2adab@users.noreply.github.com>
2023-02-28 10:10:53 +0100

    Update streamlit_app.py
    Adding a header

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,5 @@ import requests
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
+streamlit.header("Fruityvice Fruit Advice!")
